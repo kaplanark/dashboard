b5fcc7b35f00e38035344442fd84d28039a77efe
kaplanark <kaplanarkan@gmail.com>
2022-10-31 21:49:08 +0300

    added alert provider component

---
+++ src/components/Alert/AlertProvider.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps({
+	position: {
+		type: String,
+		default: 'top-right'
+	}
+});
+</script>
+
+<template>
+	<div class="alert-provider" :class="'alert-provider--' + props.position">
+		<slot></slot>
+	</div>
+</template>
+
+<style lang="scss">
+.alert-provider {
+	position: fixed;
+	z-index: 1000;
+
+	&--top-right {
+		top: 16px;
+		right: 16px;
+	}
+
+	&--top-left {
+		top: 16px;
+		left: 16px;
+	}
+
+	&--bottom-right {
+		bottom: 16px;
+		right: 16px;
+	}
+
+	&--bottom-left {
+		bottom: 16px;
+		left: 16px;
+	}
+}
+</style>
